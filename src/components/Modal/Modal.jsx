92cad73e711f1efcaba45d67670d67e71abb5f5d
Oleksandr Rusnak <san.ru.k.ar@gmail.com>
2023-04-08 16:37:52 +0200

    update

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,29 +1,27 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 const modalRef = document.querySelector('#root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.onCloseByEsc);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onCloseByEsc);
-  }
-  onCloseByEsc = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-  render() {
-    const { largeImage, type, onClose } = this.props;
-    return createPortal(
-      <div onClick={onClose} className="Overlay">
-        <div className="Modal">
-          <img src={largeImage} alt={type} />
-        </div>
-      </div>,
-      modalRef
-    );
-  }
-}
+export const Modal = ({ largeImage, type, onClose }) => {
+  useEffect(() => {
+    const onCloseByEsc = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', onCloseByEsc);
+    return () => {
+      window.removeEventListener('keydown', onCloseByEsc);
+    };
+  }, [onClose]);
+
+  return createPortal(
+    <div onClick={onClose} className="Overlay">
+      <div className="Modal">
+        <img src={largeImage} alt={type} />
+      </div>
+    </div>,
+    modalRef
+  );
+};
